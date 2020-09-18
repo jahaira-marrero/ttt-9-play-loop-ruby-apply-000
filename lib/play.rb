7ebeff1a5baf63b798177eca9696bc2fe8fbf6f4
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-09-18 02:29:07 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,15 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+  turns = 3
+  counter = 0
+  loop do
+    counter = counter + 1
+    turn(board)
+    if counter >= turns
+      puts "You only get #{turns} turns baby!"
+    end
+
+end
